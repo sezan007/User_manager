a1ece76dc16282bfe52126caf8d2557104474717
sezan <naimulsezan@gmail.com>
2024-05-04 01:10:56 +0600

    autentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
