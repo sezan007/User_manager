9272e9fac971abea0b439d7b790eb35d456de0f2
sezan <naimulsezan@gmail.com>
2024-05-04 20:21:14 +0600

    FULL N FINAL

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
   sessions: 'users/sessions',
-  registrations: 'users/registrations'
+  registrations: 'users/registrations',
+  sessions: 'users/sessions'
 }
   resources :users do
     collection do
@@ -19,7 +20,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  # root to: 'users/sessions#new'
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "pages#home"
+  root to: redirect('/users/sign_in')
 end
